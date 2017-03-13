2fd7676e2735a5273d6c77d9d4bbfd457b533cf3
Jonathan Ferreira <Jonathan Ferreira>
2017-03-13 00:52:35 -0300

    First commit

---
+++ client/lista/lista.js
@@ -0,0 +1,21 @@
+Template.lista.helpers({
+
+    tarefas: function() {
+        return Tarefas.find({});
+    },
+
+    formataData: function(){
+        return moment(this.data).format("DD/MM/YYYY HH:mm");
+    }
+
+});
+
+Template.lista.events({
+
+    "click #excluirTarefa": function(e, template){
+        var tarefa = this;
+
+        Tarefas.remove({_id : tarefa._id});
+    }
+
+});
